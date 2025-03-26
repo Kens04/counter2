d5d691f1814dc8910a19a68438a6a731644e0db1
Kens04 <kento090403@icloud.com>
2025-03-26 09:23:52 +0900

    カウントが10になった時ボタンを押せるようにしました。

--- reset.js
+++ reset.js
@@ -3,6 +3,22 @@
 
   const clickHandler = () => {
     $counter.textContent = 0;
+
+    if ($counter.textContent === "0") {
+      for (
+        let index = 0;
+        index < document.getElementsByClassName("js-button").length;
+        index++
+      ) {
+        document.getElementsByClassName("js-button")[
+          index
+        ].style.pointerEvents = "auto";
+        document.getElementsByClassName("js-button")[
+          index
+        ].style.backgroundColor = "#000";
+      }
+      return;
+    }
   };
 
   document
