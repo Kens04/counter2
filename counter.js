02d8bd65abf5dfc7f0361a44eb38cc4ca64af786
Kens04 <kento090403@icloud.com>
2025-03-26 09:22:50 +0900

    10カウントずつ増えるボタン追加カウントが10になった時アラート表示とボタンを押せないようにしました。

--- counter.js
+++ counter.js
@@ -6,8 +6,25 @@
     let currentCount = parseInt($counter.textContent);
     if ($targetButton.textContent === "+") {
       $counter.textContent = currentCount + 1;
+    } else if ($targetButton.textContent === "+10") {
+      $counter.textContent = currentCount + 10;
     } else {
       $counter.textContent = currentCount - 1;
+    }
+
+    const plusCount = currentCount + 1;
+    if (plusCount === 10 || $targetButton.textContent === "+10") {
+      alert("カウントが10になりました。");
+      for (
+        let index = 0;
+        index < document.getElementsByClassName("js-button").length;
+        index++
+      ) {
+        document.getElementsByClassName("js-button")[index].style.pointerEvents =
+          "none";
+        document.getElementsByClassName("js-button")[index].style.backgroundColor =
+          "gray";
+      }
     }
   };
 
